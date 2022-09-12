79f450ed4c3dc13c78359db314afa5e5e587e56f
abettaieb30 <amor.bettaieb@gmail.com>
2022-09-12 11:11:08 +0100

    doneee

---
+++ api/src/UserList.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {DataGrid} from "@mui/x-data-grid"
+import { useEffect,useState } from 'react'
+import axios from 'axios'
+const Users = () => {
+    const [rows, setRows] = useState([]);
+    useEffect(() => {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((response) => {
+          setRows(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }, []);
+  
+    const columns = [
+      {field: "id", headerName: "ID", width: 100},
+      {field: "name", headerName: "Name", width: 150},
+      {field: "username", headerName: "Username", width: 200},
+      {field: "email", headerName: "Email", width: 300},
+      {field: "phone", headerName: "Phone", width: 200},
+      {field: "website", headerName: "Website", width: 300},
+    ];
+    return (
+      <div className='gr' style={{height: 350, width: "80%"}}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={3}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
+    );
+  };
+  
+  export default Users;
